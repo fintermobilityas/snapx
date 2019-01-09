c81bd415458137fa8d3161febbfb672c462f5b98
Peter Rekdal Sunde <peters@users.noreply.github.com>
2019-01-09 17:54:25 +0100

    Add snapaware attribute.

---
+++ src/Snap.Core/Attributes/SnapAwareAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Snap.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public sealed class SnapAwareAttribute : Attribute
+    {
+    }
+}

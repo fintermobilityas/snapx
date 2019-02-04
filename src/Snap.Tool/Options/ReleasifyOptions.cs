a081fcb0ce1582ae4572195fd7d0eea35c289068
Peter Rekdal Sunde <peter.sunde@gmail.com>
2019-02-04 21:21:56 +0100

    Update releasify options.

--- src/Snap.Tool/Options/ReleasifyOptions.cs
+++ src/Snap.Tool/Options/ReleasifyOptions.cs
@@ -1,15 +1,18 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using CommandLine;
+using NuGet.Versioning;
 
 namespace Snap.Tool.Options
 {
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
-    [Verb("releasify", HelpText = "Create a new release for a given snap app")]
+    [Verb("releasify", HelpText = "Create a new release for a given app.")]
     internal class ReleasifyOptions : BaseSubOptions
     {
-        [Option('a', "app", HelpText = "Snap app name.", Required = true)]
+        [Option('a', "app", HelpText = "Application name.", Required = true)]
         public string App { get; set; }
-        [Option('d', "base-directory", HelpText = "Base directory for where binaries for current app can be found.", Required = true)]
-        public string BaseDirectory { get; set; }
+        [Option('d', "publish-directory", HelpText = "Location on disk where current app has been published.", Required = true)]
+        public string PublishDirectory { get; set; }
+        [Option('v', "version", HelpText = "New application version.", Required = true)]
+        public SemanticVersion Version { get; set; }
     }
 }

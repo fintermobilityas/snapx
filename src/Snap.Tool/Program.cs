18015bcc9e9332421eb58e57d90d0baab2a192f1
Peter Rekdal Sunde <peter.sunde@gmail.com>
2019-02-05 18:10:38 +0100

    Disable logger for now.

--- src/Snap.Tool/Program.cs
+++ src/Snap.Tool/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -29,7 +29,6 @@ namespace Snap.Tool
         {
             try
             {
-                LogProvider.SetCurrentLogProvider(new ColoredConsoleLogProvider(LogLevel.Info));
                 return MainImplAsync(args);
             }
             catch (Exception e)

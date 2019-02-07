5d086bb10162f245c1273d9cb2064f5470b98166
Peter Rekdal Sunde <peter.sunde@gmail.com>
2019-02-08 00:33:40 +0100

    Update pack options.

--- src/Snapx/Options/PackOptions.cs
+++ src/Snapx/Options/PackOptions.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.CodeAnalysis;
 using CommandLine;
 using JetBrains.Annotations;
 
@@ -9,11 +9,11 @@ namespace snapx.Options
     [UsedImplicitly]
     internal class PackOptions : BaseSubOptions
     {
-        [Option('a', "app", HelpText = "Application name", Required = true)]
+        [Option("id", HelpText = "Application id", Required = true)]
         public string App { get; set; }
-        [Option('r', "runtime-identifier", HelpText = "Runtime identifier target name, e.g win7-x64", Required = true)]
+        [Option('r', "rid", HelpText = "Runtime identifier target name, e.g win7-x64", Required = true)]
         public string Rid { get; set; }
-        [Option('d', "publish-directory", HelpText = "Location on disk where current app has been published (Required only if you have not specified an artifacts directory)")]
+        [Option('d', "artifacts-directory", HelpText = "Self-contained dotnet publish directory")]
         public string ArtifactsDirectory { get; set; }
         [Option('v', "version", HelpText = "New application version (Required only if we you don't have a bump strategy configured)")]
         public string Version { get; set; }

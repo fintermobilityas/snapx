1faefb657ad0940960d4db1e5b0f1df85fa20ec9
Peter Rekdal Sunde <peter.sunde@gmail.com>
2019-02-08 02:39:28 +0100

    Prevent "Snap to" resizing.

--- src/Snap.Installer/Controls/ChromeLessWindow.cs
+++ src/Snap.Installer/Controls/ChromeLessWindow.cs
@@ -1,15 +1,38 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Input;
+using JetBrains.Annotations;
 
 namespace Snap.Installer.Controls
 {
     internal class ChromeLessWindow : Window
     {
+        [UsedImplicitly]
+        public int FixedWidth { get; set; } = 800;
+        [UsedImplicitly]
+        public int FixedHeight { get; set; } = 600;
+        
         protected override void OnPointerPressed(PointerPressedEventArgs e)
         {
             BeginMoveDrag();
 
             base.OnPointerPressed(e);
+        }
+        
+        protected override void HandleResized(Size clientSize)
+        {
+            if (FixedWidth > 0 && FixedHeight > 0 
+                               && clientSize != new Size(FixedWidth, FixedHeight))
+            {
+                return;
+            }
+            
+            base.HandleResized(clientSize);
+        }
+
+        protected override void HandleWindowStateChanged(WindowState state)
+        {
+            WindowState = WindowState.Normal;
         }
     }
 }

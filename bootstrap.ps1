4a2da53a8c11832d11125ad8fa7b07afa62b9298
Peter Rekdal Sunde <peter.sunde@gmail.com>
2019-02-08 23:15:44 +0100

    Reformat powershell files.

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -12,70 +12,69 @@ param(
 
 function Out-FileUtf8NoBom {
 
-  [CmdletBinding()]
-  param(
-    [Parameter(Mandatory, Position=0)] [string] $LiteralPath,
-    [switch] $Append,
-    [switch] $NoClobber,
-    [AllowNull()] [int] $Width,
-    [Parameter(ValueFromPipeline)] $InputObject
-  )
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position = 0)] [string] $LiteralPath,
+        [switch] $Append,
+        [switch] $NoClobber,
+        [AllowNull()] [int] $Width,
+        [Parameter(ValueFromPipeline)] $InputObject
+    )
 
-  #requires -version 3
+    #requires -version 3
 
-  # Make sure that the .NET framework sees the same working dir. as PS
-  # and resolve the input path to a full path.
-  [System.IO.Directory]::SetCurrentDirectory($PWD) # Caveat: .NET Core doesn't support [Environment]::CurrentDirectory
-  $LiteralPath = [IO.Path]::GetFullPath($LiteralPath)
+    # Make sure that the .NET framework sees the same working dir. as PS
+    # and resolve the input path to a full path.
+    [System.IO.Directory]::SetCurrentDirectory($PWD) # Caveat: .NET Core doesn't support [Environment]::CurrentDirectory
+    $LiteralPath = [IO.Path]::GetFullPath($LiteralPath)
 
-  # If -NoClobber was specified, throw an exception if the target file already
-  # exists.
-  if ($NoClobber -and (Test-Path $LiteralPath)) {
-    Throw [IO.IOException] "The file '$LiteralPath' already exists."
-  }
+    # If -NoClobber was specified, throw an exception if the target file already
+    # exists.
+    if ($NoClobber -and (Test-Path $LiteralPath)) {
+        Throw [IO.IOException] "The file '$LiteralPath' already exists."
+    }
 
-  # Create a StreamWriter object.
-  # Note that we take advantage of the fact that the StreamWriter class by default:
-  # - uses UTF-8 encoding
-  # - without a BOM.
-  $sw = New-Object IO.StreamWriter $LiteralPath, $Append
+    # Create a StreamWriter object.
+    # Note that we take advantage of the fact that the StreamWriter class by default:
+    # - uses UTF-8 encoding
+    # - without a BOM.
+    $sw = New-Object IO.StreamWriter $LiteralPath, $Append
 
-  $htOutStringArgs = @{}
-  if ($Width) {
-    $htOutStringArgs += @{ Width = $Width }
-  }
+    $htOutStringArgs = @{}
+    if ($Width) {
+        $htOutStringArgs += @{ Width = $Width }
+    }
 
-  # Note: By not using begin / process / end blocks, we're effectively running
-  #       in the end block, which means that all pipeline input has already
-  #       been collected in automatic variable $Input.
-  #       We must use this approach, because using | Out-String individually
-  #       in each iteration of a process block would format each input object
-  #       with an indvidual header.
-  try {
-    $Input | Out-String -Stream @htOutStringArgs | % { $sw.WriteLine($_) }
-  } finally {
-    $sw.Dispose()
-  }
+    # Note: By not using begin / process / end blocks, we're effectively running
+    #       in the end block, which means that all pipeline input has already
+    #       been collected in automatic variable $Input.
+    #       We must use this approach, because using | Out-String individually
+    #       in each iteration of a process block would format each input object
+    #       with an indvidual header.
+    try {
+        $Input | Out-String -Stream @htOutStringArgs | % { $sw.WriteLine($_) }
+    }
+    finally {
+        $sw.Dispose()
+    }
 
 }
 
-function Die 
-{
-	param(
+function Die {
+    param(
         [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
         [string] $Message
     )
 
     Write-Host
     Write-Host $Message -ForegroundColor Red
-	Write-Host
+    Write-Host
     
-	Write-Error $Message
-	exit 0	
+    Write-Error $Message
+    exit 0	
 }
 
-function Write-Output-Header 
-{
+function Write-Output-Header {
     param(
         [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
         [string] $Message
@@ -86,8 +85,7 @@ function Write-Output-Header
     Write-Host
 }
 
-function Write-Output-Header-Warn
-{
+function Write-Output-Header-Warn {
     param(
         [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
         [string] $Message
@@ -106,7 +104,7 @@ $ToolsDir = Join-Path $WorkingDir tools
 # Global variables
 
 $DebugPrefix = ""
-if($Configuration -eq "Debug") {
+if ($Configuration -eq "Debug") {
     $DebugPrefix = "d"
 }
 
@@ -123,38 +121,36 @@ $CommandDotnet = $null
 $CommandMsBuild = $null
 $CommandMake = $null
 
-switch -regex ($OSVersion)
-{
-	"^Microsoft Windows" {
-		$OSPlatform = "Windows"
-		$CmakeGenerator = "Visual Studio 15 Win64"
-		$CommandCmake = "cmake.exe"
-		$CommandGit = "git.exe"
-		$CommandDotnet = "dotnet.exe"
+switch -regex ($OSVersion) {
+    "^Microsoft Windows" {
+        $OSPlatform = "Windows"
+        $CmakeGenerator = "Visual Studio 15 Win64"
+        $CommandCmake = "cmake.exe"
+        $CommandGit = "git.exe"
+        $CommandDotnet = "dotnet.exe"
         $CommandUpx = Join-Path $ToolsDir upx.exe
-		$Arch = "win7-x64"
-		$ArchCross = "x86_64-win64-gcc"
-	}
-	"^Unix" {
-		$OSPlatform = "Unix"
-		$CmakeGenerator = "Unix Makefiles"
-		$CommandCmake = "cmake"
-		$CommandGit = "git"
-		$CommandDotnet = "dotnet"
-		$CommandMake = "make"
+        $Arch = "win7-x64"
+        $ArchCross = "x86_64-win64-gcc"
+    }
+    "^Unix" {
+        $OSPlatform = "Unix"
+        $CmakeGenerator = "Unix Makefiles"
+        $CommandCmake = "cmake"
+        $CommandGit = "git"
+        $CommandDotnet = "dotnet"
+        $CommandMake = "make"
         $CommandUpx = "upx"
-		$Arch = "x86_64-linux-gcc"
-		$ArchCross = "x86_64-w64-mingw32-gcc"
-	}	
-	default {
-		Die "Unsupported os: $OSVersion"
-	}
+        $Arch = "x86_64-linux-gcc"
+        $ArchCross = "x86_64-w64-mingw32-gcc"
+    }	
+    default {
+        Die "Unsupported os: $OSVersion"
+    }
 }
 
 $TargetArch = $Arch
-if($Cross)
-{
-  $TargetArch = $ArchCross
+if ($Cross) {
+    $TargetArch = $ArchCross
 }
 
 # Projects
@@ -165,66 +161,55 @@ $SnapCoreRunInstallDir = Join-Path $SnapCoreRunBuildOutputDir install
 
 # Miscellaneous functions that require bootstrapped variable state
 
-function Requires-Cmake
-{
-	if ((Get-Command $CommandCmake -ErrorAction SilentlyContinue) -eq $null) {
-		Die "Unable to find cmake executable in environment path: $CommandCmake"
-	}
+function Requires-Cmake {
+    if ((Get-Command $CommandCmake -ErrorAction SilentlyContinue) -eq $null) {
+        Die "Unable to find cmake executable in environment path: $CommandCmake"
+    }
 }
 
-function Requires-Git 
-{
-	if ((Get-Command $CommandGit -ErrorAction SilentlyContinue) -eq $null) {
-		Die "Unable to find git executable in environment path: $CommandGit"
-	}
+function Requires-Git {
+    if ((Get-Command $CommandGit -ErrorAction SilentlyContinue) -eq $null) {
+        Die "Unable to find git executable in environment path: $CommandGit"
+    }
 }
 
-function Requires-Dotnet
-{
-	if ((Get-Command $CommandDotnet -ErrorAction SilentlyContinue) -eq $null) {
-		Die "Unable to find dotnet executable in environment path: $CommandDotnet"
-	}
+function Requires-Dotnet {
+    if ((Get-Command $CommandDotnet -ErrorAction SilentlyContinue) -eq $null) {
+        Die "Unable to find dotnet executable in environment path: $CommandDotnet"
+    }
 }
 
-function Requires-Msbuild
-{
-	if ((Get-Command $CommandMsBuild -ErrorAction SilentlyContinue) -eq $null) {
-		Die "Unable to find msbuild executable in environment path: $CommandMsBuild"
-	}
+function Requires-Msbuild {
+    if ((Get-Command $CommandMsBuild -ErrorAction SilentlyContinue) -eq $null) {
+        Die "Unable to find msbuild executable in environment path: $CommandMsBuild"
+    }
 }
 
-function Requires-Make
-{
-	if ((Get-Command $CommandMake -ErrorAction SilentlyContinue) -eq $null) {
-		Die "Unable to find make executable in environment path: $CommandMake"
-	}
+function Requires-Make {
+    if ((Get-Command $CommandMake -ErrorAction SilentlyContinue) -eq $null) {
+        Die "Unable to find make executable in environment path: $CommandMake"
+    }
 }
 
-function Requires-Upx
-{
-	if ((Get-Command $CommandUpx -ErrorAction SilentlyContinue) -eq $null) {
-		Die "Unable to find upx executable in environment path: $CommandUpx"
-	}
+function Requires-Upx {
+    if ((Get-Command $CommandUpx -ErrorAction SilentlyContinue) -eq $null) {
+        Die "Unable to find upx executable in environment path: $CommandUpx"
+    }
 }
 
-function Requires-Unix 
-{
-	if($OSPlatform -ne "Unix")
-	{
-		Die "Unable to continue because OS version is not Unix but $OSVersion"
-	}	
+function Requires-Unix {
+    if ($OSPlatform -ne "Unix") {
+        Die "Unable to continue because OS version is not Unix but $OSVersion"
+    }	
 }
 
-function Requires-Windows 
-{
-	if($OSPlatform -ne "Windows")
-	{
-		Die "Unable to continue because OS version is not Windows but $OSVersion"
-	}	
+function Requires-Windows {
+    if ($OSPlatform -ne "Windows") {
+        Die "Unable to continue because OS version is not Windows but $OSVersion"
+    }	
 }
 
-function Start-Process 
-{
+function Start-Process {
     param(
         [string] $Filename,
         [string[]] $Arguments
@@ -248,18 +233,17 @@ function Start-Process
     $Process.Start() | Out-Null
     $Process.WaitForExit()
 
-    if($Process.ExitCode -ne 0) {
+    if ($Process.ExitCode -ne 0) {
         Die ("{0} returned a non-zero exit code" -f $Filename)
     }
 }
 
-function Invoke-BatchFile 
-{
+function Invoke-BatchFile {
     param(
         [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
         [string]$Path, 
         [Parameter(Position = 1, Mandatory = $true, ValueFromPipeline = $true)]
-		[string]$Parameters
+        [string]$Parameters
     )
 
     $tempFile = [IO.Path]::GetTempFileName()
@@ -270,35 +254,33 @@ function Invoke-BatchFile
     $batFile | Out-Null
 
     Get-Content $tempFile | Foreach-Object {   
-		if ($_ -match "^(.*?)=(.*)$")
-		{ 
-			Set-Content "env:\$($matches[1])" $matches[2] | Out-Null
-		}	
-	}
+        if ($_ -match "^(.*?)=(.*)$") { 
+            Set-Content "env:\$($matches[1])" $matches[2] | Out-Null
+        }	
+    }
    
-   Remove-Item $tempFile | Out-Null
+    Remove-Item $tempFile | Out-Null
 }
 
-function Configure-Msvs-Toolchain
-{
-  Write-Output-Header "Configuring msvs toolchain"
+function Configure-Msvs-Toolchain {
+    Write-Output-Header "Configuring msvs toolchain"
 
-	# https://github.com/Microsoft/vswhere/commit/a8c90e3218d6c4774f196d0400a8805038aa13b1 (Release mode / VS 2015 Update 3)
-	# SHA512: 06FAE35E3A5B74A5B0971FB19EE0987E15E413558C620AB66FB3188F6BF1C790919E8B163596744D126B3716D0E91C65F7C1325F5752614078E6B63E7C81D681
-	$wswhere = Join-Path $WorkingDir Tools\vswhere
-	$VxxCommonTools = $null
+    # https://github.com/Microsoft/vswhere/commit/a8c90e3218d6c4774f196d0400a8805038aa13b1 (Release mode / VS 2015 Update 3)
+    # SHA512: 06FAE35E3A5B74A5B0971FB19EE0987E15E413558C620AB66FB3188F6BF1C790919E8B163596744D126B3716D0E91C65F7C1325F5752614078E6B63E7C81D681
+    $wswhere = Join-Path $WorkingDir Tools\vswhere
+    $VxxCommonTools = $null
 	
-	$Ids = 'Community', 'Professional', 'Enterprise', 'BuildTools' | foreach { 'Microsoft.VisualStudio.Product.' + $_ }
-	$Instance = & $wswhere -version 15 -products $ids -requires 'Microsoft.Component.MSBuild' -format json `
-		| convertfrom-json `
-		| select-object -first 1
+    $Ids = 'Community', 'Professional', 'Enterprise', 'BuildTools' | foreach { 'Microsoft.VisualStudio.Product.' + $_ }
+    $Instance = & $wswhere -version 15 -products $ids -requires 'Microsoft.Component.MSBuild' -format json `
+        | convertfrom-json `
+        | select-object -first 1
 						
-	if($Instance -eq $null) {
-		Die "Visual Studio 2017 was not found"
-	}
+    if ($Instance -eq $null) {
+        Die "Visual Studio 2017 was not found"
+    }
 		
-	$VXXCommonTools = Join-Path $Instance.installationPath VC\Auxiliary\Build
-	$script:CommandMsBuild = Join-Path $Instance.installationPath MSBuild\15.0\Bin\msbuild.exe
+    $VXXCommonTools = Join-Path $Instance.installationPath VC\Auxiliary\Build
+    $script:CommandMsBuild = Join-Path $Instance.installationPath MSBuild\15.0\Bin\msbuild.exe
 
     if ($VXXCommonTools -eq $null -or (-not (Test-Path($VXXCommonTools)))) {
         Die "PlatformToolset $PlatformToolset is not installed."
@@ -309,76 +291,76 @@ function Configure-Msvs-Toolchain
         Die "Unable to find $VCVarsAll"
     }
         
-	Invoke-BatchFile $VXXCommonTools x64
+    Invoke-BatchFile $VXXCommonTools x64
 	
     Write-Output "Successfully configured msvs"
 }
 
-function Build-SnapCoreRun
-{	
-	Write-Output-Header "Building snap.corerun"
+function Build-SnapCoreRun {	
+    Write-Output-Header "Building snap.corerun"
 
-	$CmakeArguments = @(
-		"-G`"$CmakeGenerator`"",
-  	"-H`"$SnapCoreRunSrcDir`"",
-  	"-B`"$SnapCoreRunBuildOutputDir`""
-	)
+    $CmakeArguments = @(
+        "-G`"$CmakeGenerator`"",
+        "-H`"$SnapCoreRunSrcDir`"",
+        "-B`"$SnapCoreRunBuildOutputDir`""
+    )
 	
-	if($Lto)
-	{
-		$CmakeArguments += "-DENABLE_LTO=1"
-	}
+    if ($Lto) {
+        $CmakeArguments += "-DENABLE_LTO=1"
+    }
 
-	if($Cross -eq $TRUE -and $OsPlatform -eq "Unix")
-	{
-		$CmakeToolChainFile = Join-Path $WorkingDir cmake\Toolchain-x86_64-w64-mingw32.cmake
-		$CmakeArguments += "-DCMAKE_TOOLCHAIN_FILE=$CmakeToolChainFile"
-	} elseif($Cross -eq $TRUE)
-	{
-		Die "Cross compiling is not support on: $OSPlatform"
-	}
+    if($Configuration -eq "Debug")
+    {
+        $CmakeArguments += "-DCMAKE_BUILD_TYPE=Debug"
+    } else {
+        $CmakeArguments += "-DCMAKE_BUILD_TYPE=Release"
+    }
+
+    if ($Cross -eq $TRUE -and $OsPlatform -eq "Unix") {
+        $CmakeToolChainFile = Join-Path $WorkingDir cmake\Toolchain-x86_64-w64-mingw32.cmake
+        $CmakeArguments += "-DCMAKE_TOOLCHAIN_FILE=$CmakeToolChainFile"
+    }
+    elseif ($Cross -eq $TRUE) {
+        Die "Cross compiling is not support on: $OSPlatform"
+    }
 			
-	Write-Output "Build src directory: $SnapCoreRunSrcDir"
-	Write-Output "Build output directory: $SnapCoreRunBuildOutputDir"
-	Write-Output "Build install directory: $SnapCoreRunInstallDir"
-	Write-Output "Arch: $TargetArch"
-	Write-Output ""
+    Write-Output "Build src directory: $SnapCoreRunSrcDir"
+    Write-Output "Build output directory: $SnapCoreRunBuildOutputDir"
+    Write-Output "Build install directory: $SnapCoreRunInstallDir"
+    Write-Output "Arch: $TargetArch"
+    Write-Output ""
 	
-	Write-Output $CmakeArguments
+    Write-Output $CmakeArguments
 	
-	Start-Process $CommandCmake $CmakeArguments
+    Start-Process $CommandCmake $CmakeArguments
 	
-	switch($OSPlatform)
-	{
-		"Unix" {
-			sh -c "(cd $SnapCoreRunBuildOutputDir && make -j $ProcessorCount)"
+    switch ($OSPlatform) {
+        "Unix" {
+            sh -c "(cd $SnapCoreRunBuildOutputDir && make -j $ProcessorCount)"
 
-            if($Configuration -eq "Release")
-            {
+            if ($Configuration -eq "Release") {
                 $SnapCoreRunBinary = Join-Path $SnapCoreRunBuildOutputDir Snap.CoreRun\corerun
-                if($Cross -eq $TRUE)
-                {
+                if ($Cross -eq $TRUE) {
                     $SnapCoreRunBinary = Join-Path $SnapCoreRunBuildOutputDir Snap.CoreRun\corerun.exe
                 }
                 Start-Process $CommandUpx @("--ultra-brute $SnapCoreRunBinary")
             }
 
-		}
-		"Windows" {
-			Start-Process $CommandCmake @(
-				"--build `"$SnapCoreRunBuildOutputDir`" --config $Configuration"
-			)
+        }
+        "Windows" {
+            Start-Process $CommandCmake @(
+                "--build `"$SnapCoreRunBuildOutputDir`" --config $Configuration"
+            )
         
-            if($Configuration -eq "Release")
-            {
+            if ($Configuration -eq "Release") {
                 $SnapCoreRunBinary = Join-Path $SnapCoreRunBuildOutputDir Snap.CoreRun\$Configuration\corerun.exe
                 Start-Process $CommandUpx @("--ultra-brute $SnapCoreRunBinary")
             }
-		}
-		default {
-			Die "Unsupported os platform: $OSPlatform"
-		}
-	}
+        }
+        default {
+            Die "Unsupported os platform: $OSPlatform"
+        }
+    }
 			
 }
 
@@ -389,42 +371,41 @@ Write-Output "Processor count: $ProcessorCount"
 Write-Output "Build output directory: $BuildOutputDir"
 Write-Output "Configuration: $Configuration"
 
-if($Cross) {
-	Write-Output "Target arch: $ArchCross"		
-} else {
-	Write-Output "Target arch: $Arch"				
+if ($Cross) {
+    Write-Output "Target arch: $ArchCross"		
+}
+else {
+    Write-Output "Target arch: $Arch"				
 }
 
-switch($OSPlatform)
-{
-	"Windows" {
-		Requires-Windows 							
-		Configure-Msvs-Toolchain
-		Requires-Msbuild
-	}
-	"Unix" {
-		Requires-Unix
-		Requires-Make
-	}
-	default
-	{
-		Die "Unsupported os platform: $OSPlatform"
-	}
+switch ($OSPlatform) {
+    "Windows" {
+        Requires-Windows 							
+        Configure-Msvs-Toolchain
+        Requires-Msbuild
+    }
+    "Unix" {
+        Requires-Unix
+        Requires-Make
+    }
+    default {
+        Die "Unsupported os platform: $OSPlatform"
+    }
 }
 			
 Requires-Cmake
 Requires-Upx
 
-switch($OSPlatform)
-{
-	"Windows" {		
-		if($Cross -eq $FALSE) {
-			Build-SnapCoreRun		
-		} else {
-		 	Die "Cross compiling is not supported on Windows."
-		} 
-	}
-	"Unix" {
-		Build-SnapCoreRun		
-	}
+switch ($OSPlatform) {
+    "Windows" {		
+        if ($Cross -eq $FALSE) {
+            Build-SnapCoreRun		
+        }
+        else {
+            Die "Cross compiling is not supported on Windows."
+        } 
+    }
+    "Unix" {
+        Build-SnapCoreRun		
+    }
 }
--- build.ps1
+++ build.ps1
@@ -9,8 +9,7 @@ $OSVersion = [Environment]::OSVersion
 $Stopwatch = [System.Diagnostics.Stopwatch]
 
 # Global functions
-function Write-Output-Header 
-{
+function Write-Output-Header {
     param(
         [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true)]
         [string] $Message
@@ -26,30 +25,27 @@ function Write-Output-Header
 $BuildTime = $StopWatch::StartNew()
 
 
-switch($Target)
-{
-	"Bootstrap" {
+switch ($Target) {
+    "Bootstrap" {
 		
-		if($OSVersion -match "^Microsoft Windows")
-		{
-			# NB! Install from Microsoft Store. Afterwards run install.sh
-			Ubuntu1804 run pwsh -f build.ps1 -Target Bootstrap
+        if ($OSVersion -match "^Microsoft Windows") {
+            # NB! Install from Microsoft Store. Afterwards run install.sh
+            Ubuntu1804 run pwsh -f build.ps1 -Target Bootstrap
 
-			.\bootstrap.ps1 -Configuration Debug
-			.\bootstrap.ps1 -Configuration Release -Lto 1
+            .\bootstrap.ps1 -Configuration Debug
+            .\bootstrap.ps1 -Configuration Release -Lto 1
 			
-		}
+        }
 		
-		if($OSVersion -match "^Unix")
-		{
-			.\bootstrap.ps1 -Configuration Debug
-			.\bootstrap.ps1 -Configuration Debug -Cross 1
-			.\bootstrap.ps1 -Configuration Release -Lto 1		
-			.\bootstrap.ps1 -Configuration Release -Cross 1 -Lto 1		
-		}	
+        if ($OSVersion -match "^Unix") {
+            .\bootstrap.ps1 -Configuration Debug
+            .\bootstrap.ps1 -Configuration Debug -Cross 1
+            .\bootstrap.ps1 -Configuration Release -Lto 1		
+            .\bootstrap.ps1 -Configuration Release -Cross 1 -Lto 1		
+        }	
 		
-		$BuildTime.Stop()		
-		$Elapsed = $BuildTime.Elapsed
-		Write-Output-Header "Build elapsed: $Elapsed ($OSVersion)"
-	}
+        $BuildTime.Stop()		
+        $Elapsed = $BuildTime.Elapsed
+        Write-Output-Header "Build elapsed: $Elapsed ($OSVersion)"
+    }
 }

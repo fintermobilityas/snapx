d188743d7d3561514415f4a34a4551c0e3a4cb20
Peter Rekdal Sunde <peters@users.noreply.github.com>
2019-02-14 09:57:44 +0100

    Remove test command.

--- test.cmd
+++
@@ -1,6 +0,0 @@
-REM THIS IS JUST A TEST
-
-dotnet publish src/Snap.Installer/Snap.Installer.csproj -c release -r win-x64 -f netcoreapp2.2 /p:ShowLinkerSizeComparison=true
-snapx rcedit --gui-app -f src/Snap.Installer/bin/release/netcoreapp2.2/win-x64/publish/Snap.Installer.exe
-.\tools\warp-packer.exe --arch windows-x64 --exec Snap.Installer.exe --output src/Snap.Installer/bin/release/netcoreapp2.2/win-x64/publish/Setup.exe --input_dir src/Snap.Installer/bin/release/netcoreapp2.2/win-x64/publish
-.\tools\upx.exe --ultra-brute src/Snap.Installer/bin/release/netcoreapp2.2/win-x64/publish/Setup.exe

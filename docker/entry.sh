ba5d3e5bb9c00b76f552116416b62ad37d61eb50
Peter Rekdal Sunde <peter.sunde@gmail.com>
2019-03-02 10:48:05 +0100

    Bugfix: Set environment variables in shell script

--- docker/entry.sh
+++ docker/entry.sh
@@ -4,5 +4,8 @@ groupadd -r appgroup -g $SNAPX_DOCKER_GROUP_ID
 useradd -u $SNAPX_DOCKER_USER_ID -r -g appgroup -d /home/$SNAPX_DOCKER_USERNAME -m -s /bin/bash -c "App user" $SNAPX_DOCKER_USERNAME
 
 export SNAPX_DOCKER_BUILD=1
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
+export NUGET_XMLDOC_MODE="skip"
 
 exec su -m $SNAPX_DOCKER_USERNAME -c "(cd /build/snapx && /usr/bin/pwsh -f build.ps1 -Target Bootstrap)"

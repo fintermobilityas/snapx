72a6f43dfc94c5a3f53bcc97eb53903f58446798
Peter Rekdal Sunde <peter.sunde@gmail.com>
2019-03-22 04:30:47 +0100

    Update text

--- src/Snapx/Options/PackOptions.cs
+++ src/Snapx/Options/PackOptions.cs
@@ -11,11 +11,11 @@ namespace snapx.Options
     {
         [Option("id", HelpText = "Application id", Required = true)]
         public string AppId { get; [UsedImplicitly] set; }
-        [Option('r', "rid", HelpText = "Runtime identifier target name, e.g win7-x64", Required = true)]
+        [Option('r', "rid", HelpText = "Runtime identifier target name, e.g win-x64", Required = true)]
         public string Rid { get; [UsedImplicitly] set; }
         [Option('v', "version", HelpText = "New application version", Required = true)]
         public string Version { get; [UsedImplicitly] set; }
-        [Option('y', "yes", HelpText = "Answer yes to all prompts")]
+        [Option('y', "yes", HelpText = "Yes (y) to all prompts")]
         public bool YesToAllPrompts { get; [UsedImplicitly] set; }
     }
 }

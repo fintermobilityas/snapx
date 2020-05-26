a3ee177b75f7c621fea39e3e9ee88734ad7b0868
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-05-26 23:17:38 +0200

    Do not write temp settings to disk.

--- src/Snap/NuGet/NugetTempSettings.cs
+++ src/Snap/NuGet/NugetTempSettings.cs
@@ -1,15 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using NuGet.Configuration;
 
 namespace Snap.NuGet
 {
-    internal sealed class NugetTempSettings : Settings
+    internal sealed class NugetTempSettings : ISettings
     {
-        public NugetTempSettings([NotNull] string tempDirectory) :
-            base(tempDirectory, $"snap_nuget_temp_settings{Guid.NewGuid()}.config", false)
+        readonly Settings _settings;
+
+        public NugetTempSettings([NotNull] string tempDirectory) 
         {
             if (tempDirectory == null) throw new ArgumentNullException(nameof(tempDirectory));
+            _settings = new Settings(tempDirectory);
         }
+
+        public SettingSection GetSection(string sectionName)
+        {
+            return _settings.GetSection(sectionName);
+        }
+
+        public void AddOrUpdate(string sectionName, SettingItem item)
+        {
+            _settings.AddOrUpdate(sectionName, item);
+        }
+
+        public void Remove(string sectionName, SettingItem item)
+        {
+            _settings.Remove(sectionName, item);
+        }
+
+        public void SaveToDisk()
+        {
+            
+        }
+
+        public IList<string> GetConfigFilePaths()
+        {
+            return _settings.GetConfigFilePaths();
+        }
+
+        public IList<string> GetConfigRoots()
+        {
+            return _settings.GetConfigRoots();
+        }
+
+        public event EventHandler SettingsChanged;
     }
 }

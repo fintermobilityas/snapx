e33c14730bbb8450ff19ddd1a43962066906671f
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-09-21 23:52:34 +0200

    Fix incorrect application shortcut directory on Raspberry Pi Os

--- src/Snap/AnyOS/Unix/SnapOS.Unix.cs
+++ src/Snap/AnyOS/Unix/SnapOS.Unix.cs
@@ -139,13 +139,14 @@ namespace Snap.AnyOS.Unix
 
             string applicationsDirectoryAbsolutePath;
             string autoStartDirectoryAbsolutePath;
-            if (DistroType == SnapOsDistroType.Ubuntu)
+            if (DistroType == SnapOsDistroType.Ubuntu
+                || DistroType == SnapOsDistroType.RaspberryPi))
             {
                 applicationsDirectoryAbsolutePath = Filesystem.PathCombine($"/home/{Username}", ".local/share/applications");
                 autoStartDirectoryAbsolutePath = Filesystem.PathCombine($"/home/{Username}", ".config/autostart");
             } else if (DistroType == SnapOsDistroType.RaspberryPi)
             {
-                applicationsDirectoryAbsolutePath = Filesystem.PathCombine($"/home/{Username}", "Desktop");
+                applicationsDirectoryAbsolutePath = Filesystem.PathCombine($"/home/{Username}", ".local/share/applications");
                 autoStartDirectoryAbsolutePath = Filesystem.PathCombine($"/home/{Username}", ".config/autostart");
             }
             else

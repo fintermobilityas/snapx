0ef85e06b55b331d085afd00d42153c7f7f08af6
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-09-21 23:10:53 +0200

    Add additional test cases when performing bsdiff.

--- src/Snap.Tests/Core/SnapBinaryPatcherTests.cs
+++ src/Snap.Tests/Core/SnapBinaryPatcherTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using Snap.Core;
@@ -7,19 +8,61 @@ namespace Snap.Tests.Core
 {
     public class SnapBinaryPatcherTests
     {
+        static readonly Random Random = new Random();
+
         [Fact]
-        public async Task HandleBsDiffWithoutExtraData()
+        public async Task TestBsDiff()
+        {
+            var baseFileData = new byte[] { 0, 2, 3, 5 };
+            var newFileData = new byte[] { 0, 1, 2, 3, 10 };
+
+            await using var patchOut = new MemoryStream();
+            await SnapBinaryPatcher.CreateAsync(baseFileData, newFileData, patchOut, default);
+            var patchData = patchOut.ToArray();
+
+            await using var toPatch = new MemoryStream(baseFileData);
+            await using var patched = new MemoryStream();
+            await SnapBinaryPatcher.ApplyAsync(toPatch, async () => await Task.FromResult(new MemoryStream(patchData)), patched, default);
+
+            Assert.Equal(newFileData, patched.ToArray());
+        }
+
+        [Fact]
+        public async Task TestBsDiffRandomNoise()
+        {
+            var baseFileData = new byte[1024 * 1024];
+            Random.NextBytes(baseFileData);
+            var newFileData = new byte[1024 * 1024];
+            baseFileData.CopyTo(newFileData, 0);
+
+            for (var i = 0; i < newFileData.Length; i++)
+            {
+                if (Random.NextDouble() > 0.5)
+                {
+                    newFileData[i] = (byte)Random.Next();
+                }
+            }
+
+            await using var patchOut = new MemoryStream();
+            await SnapBinaryPatcher.CreateAsync(baseFileData, newFileData, patchOut, default);
+            var patchData = patchOut.ToArray();
+
+            await using var toPatch = new MemoryStream(baseFileData);
+            await using var patched = new MemoryStream();
+            await SnapBinaryPatcher.ApplyAsync(toPatch, async () => await Task.FromResult(new MemoryStream(patchData)), patched, default);
+
+            Assert.Equal(newFileData, patched.ToArray());
+        }
+
+        [Fact]
+        public async Task TestBsDiffWithoutExtraData()
         {
             var baseFileData = new byte[] { 1, 1, 1, 1 };
             var newFileData = new byte[] { 2, 1, 1, 1 };
 
-            byte[] patchData;
-
-            await using (var patchOut = new MemoryStream())
-            {
-                await SnapBinaryPatcher.CreateAsync(baseFileData, newFileData, patchOut, default);
-                patchData = patchOut.ToArray();
-            }
+            await using var patchOut = new MemoryStream();
+            await SnapBinaryPatcher.CreateAsync(baseFileData, newFileData, patchOut, default);
+            var patchData = patchOut.ToArray();
 
             await using var toPatch = new MemoryStream(baseFileData);
             await using var patched = new MemoryStream();

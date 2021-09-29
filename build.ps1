f0739e5c8348f5570b3135545499c3dd7f1569b0
Peter Rekdal Khan-Sunde <peter.sunde@gmail.com>
2021-09-29 17:02:33 +0200

    Update docker image.

--- build.ps1
+++ build.ps1
@@ -8,7 +8,7 @@ param(
     [Parameter(Position = 2, ValueFromPipelineByPropertyName = $true)]
 	[string] $DockerImageName = "snapx",
 	[Parameter(Position = 3, ValueFromPipelineByPropertyName = $true)]
-	[string] $DockerVersion = "9.0",
+	[string] $DockerVersion = "10.0",
 	[Parameter(Position = 4, ValueFromPipelineByPropertyName = $true)]
     [switch] $DockerLocal,
     [Parameter(Position = 5, ValueFromPipelineByPropertyName = $true)]

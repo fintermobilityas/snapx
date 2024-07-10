337ea1744757301b29beb151245ee5474c33c96c
Peter Rekdal Khan-Sunde <peter.sunde@gmail.com>
2024-07-10 10:26:34 +0200

    Update .NET sdks.

--- build.ps1
+++ build.ps1
@@ -8,7 +8,7 @@ param(
     [Parameter(Position = 2, ValueFromPipelineByPropertyName = $true)]
 	[string] $DockerImageName = "snapx",
 	[Parameter(Position = 3, ValueFromPipelineByPropertyName = $true)]
-	[string] $DockerVersion = "34.0",
+	[string] $DockerVersion = "35.0",
 	[Parameter(Position = 4, ValueFromPipelineByPropertyName = $true)]
     [switch] $DockerLocal,
     [Parameter(Position = 5, ValueFromPipelineByPropertyName = $true)]

697887d66d5fcb668f186759b9e5c757d19b3752
Peter Rekdal Khan-Sunde <peter.sunde@gmail.com>
2024-11-18 15:56:14 +0100

    Upgrade the project to .NET 9.0 (General Availability) and remove support for .NET 6.0, which reached its End of Life (EOL) on November 12, 2024.

--- src/Snap.Shared.Tests/BaseFixture.cs
+++ src/Snap.Shared.Tests/BaseFixture.cs
@@ -121,7 +121,7 @@ public class BaseFixture
             ],
             Target = new SnapTarget
             {
-                Framework = "net8.0",
+                Framework = "net9.0",
                 Shortcuts =
                 [
                     SnapShortcutLocation.Desktop,

426e8f634329125416f75a3d6d2b98757dc89bfa
Peter Rekdal Khan-Sunde <peter.sunde@gmail.com>
2025-09-19 11:35:46 +0200

    Bugfix: Typo in parameter declaration

--- src/Snap.CoreRun/src/main.cpp
+++ src/Snap.CoreRun/src/main.cpp
@@ -7,7 +7,7 @@ int APIENTRY wWinMain(
     _In_ HINSTANCE h_instance,
     _In_opt_ HINSTANCE h_prev_instance,
     _In_ LPWSTR lp_cmd_line,
-    _In_ constint n_cmd_show) {
+    _In_ const int n_cmd_show) {
     this_exe::plog_init();
 
     pal_mitigate_dll_hijacking();
--- src/Snap.CoreRun/src/main_demoapp.cpp
+++ src/Snap.CoreRun/src/main_demoapp.cpp
@@ -86,7 +86,7 @@ int APIENTRY wWinMain(
     _In_ HINSTANCE h_instance,
     _In_opt_ HINSTANCE h_prev_instance,
     _In_ LPWSTR    lp_cmd_line,
-    _In_ constint n_cmd_show)
+    _In_ const int n_cmd_show)
 // ReSharper enable all
 {
     auto argc = 0;
